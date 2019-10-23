5a5ea609db563dc927d1c2fc4f35de9c78446213
Josh Komoroske <jdkomo@gmail.com>
2019-10-22 23:25:44 -0700

    Refactor to enable ingesting a reader directly.

--- parse.go
+++ parse.go
@@ -9,10 +9,11 @@ import (
 	"encoding/xml"
 	"errors"
 	"html"
+	"io"
 )
 
-// reparentXML will wrap the given data (which is assumed to be valid XML), in
-// a fake root nodeAlias.
+// reparentXML will wrap the given reader (which is assumed to be valid XML),
+// in a fake root nodeAlias.
 //
 // This action is useful in the event that the original XML document does not
 // have a single root nodeAlias, which is required by the XML specification.
@@ -20,8 +21,12 @@ import (
 // that is encountered, which can lead to data loss from a parser perspective.
 // This function also enables the ingestion of blank XML files, which would
 // normally cause a parsing error.
-func reparentXML(data []byte) []byte {
-	return append(append([]byte("<fake-root>"), data...), "</fake-root>"...)
+func reparentXML(reader io.Reader) io.Reader {
+	return io.MultiReader(
+		bytes.NewReader([]byte("<fake-root>")),
+		reader,
+		bytes.NewReader([]byte("</fake-root>")),
+	)
 }
 
 // extractContent parses the raw contents from an XML node, and returns it in a
@@ -96,10 +101,9 @@ func extractContent(data []byte) ([]byte, error) {
 
 // parse unmarshalls the given XML data into a graph of nodes, and then returns
 // a slice of all top-level nodes.
-func parse(data []byte) ([]xmlNode, error) {
+func parse(reader io.Reader) ([]xmlNode, error) {
 	var (
-		buf  = bytes.NewBuffer(reparentXML(data))
-		dec  = xml.NewDecoder(buf)
+		dec  = xml.NewDecoder(reparentXML(reader))
 		root xmlNode
 	)
 
